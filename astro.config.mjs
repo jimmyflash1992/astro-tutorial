4ef6bf4de89624494083ab3ba7097614b29b71f3
jimmyflash1992 <jaymesrath@gmail.com>
2025-03-24 15:15:43 +1100

    complete step 5 of tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jaymes-astro-tut.netlify.app/",
+  integrations: [preact()]
+});
